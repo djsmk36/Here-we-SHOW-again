85c278a981d6030a59738cf3c6e42d3426c60ea3
Edward Valdivia Jr <evvaldiv@gmail.com>
2020-09-03 14:53:17 -0500

    homework tweaks

--- main.js
+++ main.js
@@ -1,278 +1,238 @@
+const parentElement= document.querySelector('.katas-list')
+
+//kata1 
+const kata1heading=document.createElement('h3')
+const kata1Results=document.createElement('p')
+parentElement.append(kata1heading)
+parentElement.append(kata1Results)
+
+kata1heading.append('KATA 1' )
+
+for (let counter1=1; counter1 <=20; counter1+=1){ 
+    kata1Results.append(counter1 + ', ')
+  }
+
+//kata2 
+const kata2heading=document.createElement('h3')
+const kata2Results=document.createElement('p')
+parentElement.append(kata2heading)
+parentElement.append(kata2Results)
+
+kata2heading.append('KATA 2' )
+
+for(let counter2=2; counter2<=20; counter2 +=2){
+    kata2Results.append(counter2 + ', ')
+  } 
+
+//kata3 
+const kata3heading=document.createElement('h3')
+const kata3Results=document.createElement('p')
+parentElement.append(kata3heading)
+parentElement.append(kata3Results)
+
+kata3heading.append('KATA 3' )
+
+for(let counter3=1; counter3<=19; counter3 +=2){
+    kata3Results.append(counter3 + ', ')
+  } 
+
+//kata4 
+const kata4heading=document.createElement('h3')
+const kata4Results=document.createElement('p')
+parentElement.append(kata4heading)
+parentElement.append(kata4Results)
+
+kata4heading.append('KATA 4' )
+
+for(let counter4=5; counter4<=100; counter4 +=5){
+    kata4Results.append(counter4 + ', ')
+  } 
+
+//kata 5
+const kata5heading=document.createElement('h3')
+const kata5Results=document.createElement('p')
+parentElement.append(kata5heading)
+parentElement.append(kata5Results)
+
+kata5heading.append('KATA 5' )
+
+//number is passed - if statement only displays sq NO#
+for(let counter5=1; counter5<=100; counter5 +=1){
+    const perfectSquare=Number.isInteger(Math.sqrt(counter5))
+
+    if (perfectSquare){
+kata5Results.append(counter5 + ', ')
+    }
+  } 
+
+//kata 6
+const kata6heading=document.createElement('h3')
+const kata6Results=document.createElement('p')
+parentElement.append(kata6heading)
+parentElement.append(kata6Results)
+
+kata6heading.append('KATA 6' )
+
+for(let counter6=20; counter6>=1; counter6 -=1){
+kata6Results.append(counter6 + ', ')
+} 
+
+//kata 7
+const kata7heading=document.createElement('h3')
+const kata7Results=document.createElement('p')
+parentElement.append(kata7heading)
+parentElement.append(kata7Results)
+
+kata7heading.append('KATA 7' )
+
+for(let counter7=20; counter7>=1; counter7 -=2){
+kata7Results.append(counter7 + ', ')
+  } 
+
+//kata 8
+const kata8heading=document.createElement('h3')
+const kata8Results=document.createElement('p')
+parentElement.append(kata8heading)
+parentElement.append(kata8Results)
+
+kata8heading.append('KATA 8' )
+
+for(let counter8=19; counter8>=1; counter8 -=2){
+kata8Results.append(counter8 + ', ')
+  } 
+
+//kata 9
+const kata9heading=document.createElement('h3')
+const kata9Results=document.createElement('p')
+parentElement.append(kata9heading)
+parentElement.append(kata9Results)
+
+kata9heading.append('KATA 9' )
+
+for(let counter9=100; counter9>=5; counter9 -=5){
+kata9Results.append(counter9 + ', ')
+  } 
+
+//kata 10
+const kata10heading=document.createElement('h3')
+const kata10Results=document.createElement('p')
+parentElement.append(kata10heading)
+parentElement.append(kata10Results)
+
+kata10heading.append('KATA 10' )
+
+for(let counter10=100; counter10>=1; counter10 -=1){
+    const perfectSquareBack=Number.isInteger(Math.sqrt(counter10))
+if(perfectSquareBack)
+    kata10Results.append(counter10 + ', ')
+  }
+  
+//kata 11
+const kata11heading=document.createElement('h3')
+const kata11Results=document.createElement('p')
+parentElement.append(kata11heading)
+parentElement.append(kata11Results)
+
+kata11heading.append('KATA 11' )
+
 const sampleArray = [
     469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 
     456, 21, 398, 339, 882, 848, 179, 535, 940, 472,
 ];
 
-let documentBody = document.querySelector("body")
+for(let counter11=0; counter11<sampleArray.length; counter11 +=1){
+    kata11Results.append(sampleArray[counter11] + ', ')
+  }
+  
 
+//kata 12
+const kata12heading=document.createElement('h3')
+const kata12Results=document.createElement('p')
+parentElement.append(kata12heading)
+parentElement.append(kata12Results)
 
-//KATAS
-//#1
-let Kata1Div = document.createElement(`div`);
-Kata1Div.classList.add(`KataDiv`)
-documentBody.append(Kata1Div)
-let Kata1Title = document.createElement(`h1`);
-Kata1Title.classList.add(`KataTitle`)
-Kata1Title.append("Kata #1")
-Kata1Div.append(Kata1Title)
+kata12heading.append('KATA 12' )
 
-for (let index1 = 1; index1  <= 20; index1++) {
-    let currentNumber = index1;
-    Kata1Div.append(`${currentNumber}, `)
+for(let counter12=0; counter12<sampleArray.length; counter12 +=1){
+    if(sampleArray[counter12]%2===0){
+    kata12Results.append(sampleArray[counter12] + ', ')
+    }
+  }
+
+//kata 13
+const kata13heading=document.createElement('h3')
+const kata13Results=document.createElement('p')
+parentElement.append(kata13heading)
+parentElement.append(kata13Results)
+
+kata13heading.append('KATA 13' )
+
+for(let counter13=0; counter13<sampleArray.length; counter13 +=1){
+    if(sampleArray[counter13]%2!==0){
+    kata13Results.append(sampleArray[counter13] + ', ')
+    }
+  }
+
+  //kata 14
+const kata14heading=document.createElement('h3')
+const kata14Results=document.createElement('p')
+parentElement.append(kata14heading)
+parentElement.append(kata14Results)
+
+kata14heading.append('KATA 14' )
+
+for(let counter14=0; counter14<sampleArray.length; counter14 +=1){
     
-}
-
-//#2
-let Kata2Div = document.createElement(`div`);
-Kata2Div.classList.add(`KataDiv`)
-documentBody.append(Kata2Div)
-let Kata2Title = document.createElement(`h1`);
-Kata2Title.classList.add(`KataTitle`)
-Kata2Title.append("Kata #2")
-Kata2Div.append(Kata2Title)
-
-for (let index2 = 2; index2 <= 20; index2 += 2) {
-    let currentNumber = index2;
-    Kata2Div.append(`${currentNumber}, `)
-}
-
-//#3
-let Kata3Div = document.createElement(`div`);
-Kata3Div.classList.add(`KataDiv`)
-documentBody.append(Kata3Div)
-let Kata3Title = document.createElement(`h1`);
-Kata3Title.classList.add(`KataTitle`)
-Kata3Title.append("Kata #3")
-Kata3Div.append(Kata3Title)
-
-for (let index3 = 1; index3 <= 20; index3 += 2) {
-    let currentNumber = index3;
-    Kata3Div.append(`${currentNumber}, `)
-}
-
-//#4
-let Kata4Div = document.createElement(`div`);
-Kata4Div.classList.add(`KataDiv`)
-documentBody.append(Kata4Div)
-let Kata4Title = document.createElement(`h1`);
-Kata4Title.append("Kata #4")
-Kata4Title.classList.add(`KataTitle`)
-Kata4Div.append(Kata4Title)
-
-for (let index4 = 5; index4 <= 100; index4 += 5) {
-    let currentNumber = index4;
-    Kata4Div.append(`${currentNumber}, `)
-}
-
-//#5
-let Kata5Div = document.createElement(`div`);
-Kata5Div.classList.add(`KataDiv`)
-documentBody.append(Kata5Div)
-let Kata5Title = document.createElement(`h1`);
-Kata5Title.classList.add(`KataTitle`)
-Kata5Title.append("Kata #5")
-Kata5Div.append(Kata5Title)
-
-for (let index5 = 1; index5 <= 10; index5++) {
-    let currentSquare = index5 * index5;
-    Kata5Div.append(`${currentSquare}, `)
-}
-
-//#6
-let Kata6Div = document.createElement(`div`);
-Kata6Div.classList.add(`KataDiv`)
-documentBody.append(Kata6Div)
-let Kata6Title = document.createElement(`h1`);
-Kata6Title.classList.add(`KataTitle`)
-Kata6Title.append("Kata #6")
-Kata6Div.append(Kata6Title)
-
-for (let index6 = 20; index6 >= 1; index6 -- ) {
-    let currentNumber = index6;
-    Kata6Div.append(`${currentNumber}, `)
-}
-
-//#7
-let Kata7Div = document.createElement(`div`);
-Kata7Div.classList.add(`KataDiv`)
-documentBody.append(Kata7Div)
-let Kata7Title = document.createElement(`h1`);
-Kata7Title.classList.add(`KataTitle`)
-Kata7Title.append("Kata #7")
-Kata7Div.append(Kata7Title)
-
-for (let counteven = 20; counteven >= 2; counteven -= 2) {
-    let evenNum = counteven;
-    Kata7Div.append(evenNum + ' , ')
-}
-
-//#8
-let Kata8Div = document.createElement(`div`);
-Kata8Div.classList.add(`KataDiv`)
-documentBody.append(Kata8Div)
-let Kata8Title = document.createElement(`h1`);
-Kata8Title.classList.add(`KataTitle`)
-Kata8Title.append("Kata #8")
-Kata8Div.append(Kata8Title)
-
-for (let counteven = 19; counteven >= 1; counteven -= 2) {
-    let evenNum = counteven;
-    Kata8Div.append(evenNum + ' , ')
-}
-
-//#9
-let Kata9Div = document.createElement(`div`);
-Kata9Div.classList.add(`KataDiv`)
-documentBody.append(Kata9Div)
-let Kata9Title = document.createElement(`h1`);
-Kata9Title.classList.add(`KataTitle`)
-Kata9Title.append("Kata #9")
-Kata9Div.append(Kata9Title)
-
-for (let index9 = 100; index9 >= 5; index9 -= 5) {
-    let currentNumber = index9;
-    Kata9Div.append(`${currentNumber}, `)
-}
-
-//#10
-let Kata10Div = document.createElement(`div`);
-Kata10Div.classList.add(`KataDiv`)
-documentBody.append(Kata10Div)
-let Kata10Title = document.createElement(`h1`);
-Kata10Title.classList.add(`KataTitle`)
-Kata10Title.append("Kata #10")
-Kata10Div.append(Kata10Title)
-
-for (let index5 = 10; index5 >= 1; index5--) {
-    let currentSquare = index5 * index5;
-    Kata10Div.append(`${currentSquare}, `)
-}
-
-//#11
-let Kata11Div = document.createElement(`div`);
-Kata11Div.classList.add(`KataDiv`)
-documentBody.append(Kata11Div)
-let Kata11Title = document.createElement(`h1`);
-Kata11Title.classList.add(`KataTitle`)
-Kata11Title.append("Kata #11")
-Kata11Div.append(Kata11Title)
-
-for (let index11 = 0; index11 < sampleArray.length; index11++){
-    
-        Kata11Div.append(sampleArray[index11] + ' ,')
-    
-}
-
-//#12
-let Kata12Div = document.createElement(`div`);
-Kata12Div.classList.add(`KataDiv`)
-documentBody.append(Kata12Div)
-let Kata12Title = document.createElement(`h1`);
-Kata12Title.classList.add(`KataTitle`)
-Kata12Title.append("Kata #12")
-Kata12Div.append(Kata12Title)
-
-for (let index12 = 0; index12 <= sampleArray.length; index12++){
-    if (sampleArray[index12] % 2 === 0){
-        Kata12Div.append(sampleArray[index12] + ' ,')
+    kata14Results.append(sampleArray[counter14] + sampleArray[counter14] + ', ')
     }
-}
+  
+  //kata 15
+  const kata15heading=document.createElement('h3')
+  const kata15Results=document.createElement('p')
+  parentElement.append(kata15heading)
+  parentElement.append(kata15Results)
+  let sum=0
+  kata15heading.append('KATA 15' )
+  
+  for(let counter15=1; counter15<=20; counter15 +=1){
+      sum+=counter15
+      }
+kata15Results.append(sum)
 
-//#13
-let Kata13Div = document.createElement(`div`);
-Kata13Div.classList.add(`KataDiv`)
-documentBody.append(Kata13Div)
-let Kata13Title = document.createElement(`h1`);
-Kata13Title.classList.add(`KataTitle`)
-Kata13Title.append("Kata #13")
-Kata13Div.append(Kata13Title)
+//kata 16
+  const kata16heading=document.createElement('h3')
+  const kata16Results=document.createElement('p')
+  parentElement.append(kata16heading)
+  parentElement.append(kata16Results)
+  let sumArr=0
 
-for (let index13 = 1; index13 <= sampleArray.length; index13++){
-    if (sampleArray[index13] % 2 === 0){
-        Kata13Div.append(sampleArray[index13] + ' ,')
-    }
-}
+  kata16heading.append('KATA 16' )
+  
+  for(let counter16=0; counter16<sampleArray.length; counter16 +=1){
+      sumArr+=sampleArray[counter16]
+      } 
+     kata16Results.append(sumArr)
 
-//#14
-let Kata14Div = document.createElement(`div`);
-Kata14Div.classList.add(`KataDiv`)
-documentBody.append(Kata14Div)
-let Kata14Title = document.createElement(`h1`);
-Kata14Title.classList.add(`KataTitle`)
-Kata14Title.append("Kata #14")
-Kata14Div.append(Kata14Title)
+     //kata 17
+  const kata17heading=document.createElement('h3')
+  const kata17Results=document.createElement('p')
+  parentElement.append(kata17heading)
+  parentElement.append(kata17Results)
 
-for (let index14 = 0; index14 <= sampleArray.length; index14++){
-    if (sampleArray[index14] % 2 === 0){
-        Kata14Div.append(sampleArray[index14] + ' ,')
-    }
-}
+  kata17heading.append('KATA 17' )
 
-//#15
-let Kata15Div = document.createElement(`div`);
-Kata15Div.classList.add(`KataDiv`)
-documentBody.append(Kata15Div)
-let Kata15Title = document.createElement(`h1`);
-Kata15Title.classList.add(`KataTitle`)
-Kata15Title.append("Kata #15")
-Kata15Div.append(Kata15Title)
+      let minNum=Math.min(...sampleArray)
+      
+     kata17Results.append(minNum)
 
-let currentNumber = 0
-for (let index15 = 1; index15 <= 20; index15++){
-    currentNumber = currentNumber + index15;
-    Kata15Div.append(currentNumber + ' ,')
-}
+        //kata 18
+  const kata18heading=document.createElement('h3')
+  const kata18Results=document.createElement('p')
+  parentElement.append(kata18heading)
+  parentElement.append(kata18Results)
 
+  kata18heading.append('KATA 18' )
 
-
-//#16
-let Kata16Div = document.createElement(`div`);
-Kata16Div.classList.add(`KataDiv`)
-documentBody.append(Kata16Div)
-let Kata16Title = document.createElement(`h1`);
-Kata16Title.classList.add(`KataTitle`)
-Kata16Title.append("Kata #16")
-Kata16Div.append(Kata16Title)
-
-for (let index16 = 0; index16 <= sampleArray.length; index16++){
-    if (sampleArray[index16] % 2 === 0){
-        Kata16Div.append(sampleArray[index16] + ' ,')
-    }
-}
-
-
-
-
-//#17
-let Kata17Div = document.createElement(`div`);
-Kata17Div.classList.add(`KataDiv`)
-documentBody.append(Kata17Div)
-let Kata17Title = document.createElement(`h1`);
-Kata17Title.classList.add(`KataTitle`)
-Kata17Title.append("Kata #17")
-Kata17Div.append(Kata17Title)
-
-for (let index17 = 0; index17 <= sampleArray.length; index17++){
-    if (sampleArray[index17] % 2 === 0){
-        Kata17Div.append(sampleArray[index17] + ' ,')
-    }
-}
-
-
-
-
-
-//#18
-let Kata18Div = document.createElement(`div`);
-Kata18Div.classList.add(`KataDiv`)
-documentBody.append(Kata18Div)
-let Kata18Title = document.createElement(`h1`);
-Kata18Title.classList.add(`KataTitle`)
-Kata18Title.append("Kata #18")
-Kata18Div.append(Kata18Title)
-
-for (let index18 = 0; index18 <= sampleArray.length; index18++){
-    if (sampleArray[index18] % 2 === 0){
-        Kata18Div.append(sampleArray[index18] + ' ,')
-    }
-}
+      let maxNum=Math.max(...sampleArray)
+      
+     kata18Results.append(maxNum)
